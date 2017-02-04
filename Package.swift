9e69584581a4d86aa95628d0a59df2dc526984eb
Rien <rien@balancingrock.nl>
2017-02-04 13:47:14 +0100

    Updated VJson

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwifterLog",
     dependencies: [
-        .Package(url: "http://github.com/Balancingrock/SwifterJSON", "0.9.14"),
+        .Package(url: "http://github.com/Balancingrock/SwifterJSON", "0.9.15"),
         .Package(url: "http://github.com/Balancingrock/SwifterSockets", "0.9.12")
     ]
 )
