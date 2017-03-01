c153c99d830da7c77cd4ba20014dd744adc4d332
Balancingrock <github@balancingrock.nl>
2017-03-01 16:40:27 +0100

    Update for SwifterJSON 0.9.17

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "SwifterLog",
     dependencies: [
         .Package(url: "https://github.com/Balancingrock/CAsl", "0.1.0"),
-        .Package(url: "https://github.com/Balancingrock/SwifterJSON", "0.9.16"),
+        .Package(url: "../SwifterJSON", "0.9.17"),
         .Package(url: "../SwifterSockets", "0.9.15")
     ]
 )
